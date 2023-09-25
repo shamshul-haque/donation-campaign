ed7703683b0d0975762d7c511a79e6131eeed740
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-25 13:33:31 +0600

    added banner component with search bar

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,7 +1,30 @@
+import banner from "../../assets/images/banner.png";
 const Banner = () => {
   return (
-    <div>
-      <p>Banner</p>
+    <div
+      className="hero"
+      style={{
+        backgroundImage: `url(${banner})`,
+      }}
+    >
+      <div className="hero-overlay bg-white bg-opacity-90"></div>
+      <div className="hero-content text-center">
+        <div className="py-40">
+          <h1 className="mb-5 text-5xl font-bold">
+            I Grow By Helping People In Need
+          </h1>
+          <form action="">
+            <input
+              type="text"
+              placeholder="Search here...."
+              className="w-1/2 border border-gray-200 outline-0 p-2 rounded-md"
+            />
+            <button className="bg-red-500 p-2 text-white rounded-r-md -ml-16 font-semibold">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

e35b2fbe52a53c8a6d92044856b614f7acbda765
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-26 23:10:30 +0600

    implemented search functionalities

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,5 +1,16 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
 import banner from "../../assets/images/banner.png";
-const Banner = () => {
+
+const Banner = ({ setSearchQuery }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchQuery(inputValue);
+    setInputValue("");
+  };
+
   return (
     <div
       className="hero"
@@ -13,13 +24,19 @@ const Banner = () => {
           <h1 className="mb-5 text-5xl font-bold">
             I Grow By Helping People In Need
           </h1>
-          <form action="">
+          <form onSubmit={handleSearch}>
             <input
               type="text"
               placeholder="Search here...."
+              name="search"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
               className="w-1/2 border border-gray-200 outline-0 p-2 rounded-md"
             />
-            <button className="bg-red-500 p-2 text-white rounded-r-md -ml-16 font-semibold">
+            <button
+              className="bg-red-500 p-2 text-white rounded-r-md -ml-16 font-semibold"
+              type="submit"
+            >
               Search
             </button>
           </form>
@@ -27,6 +44,10 @@ const Banner = () => {
       </div>
     </div>
   );
+};
+
+Banner.propTypes = {
+  setSearchQuery: PropTypes.func.isRequired,
 };
 
 export default Banner;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react";
 import Banner from "../banner/Banner";
-import Categories from "../donation/Categories";
+import Category from "../donation/Category";
+
 const Home = () => {
+  const [categories, setCategories] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  useEffect(() => {
+    const loadData = async () => {
+      const res = await fetch("../donationData.json");
+      const data = await res.json();
+      setCategories(data);
+    };
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery) {
+      const filter = categories.filter(
+        (filter) => filter.Category.toLowerCase() === searchQuery.toLowerCase()
+      );
+      setCategories(filter);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchQuery]);
+
   return (
     <div>
-      <Banner />
-      <Categories />
+      <Banner searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <div className="px-5 md:px-10 lg:px-16 py-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+          {categories.map((category) => (
+            <Category key={category.id} category={category} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

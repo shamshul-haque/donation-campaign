c19f66f18981a8393d1b8b08934080cda8f05657
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-09-27 17:11:13 +0600

    back from error page implement by useNavigate func

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,4 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen space-y-2">
       <h2 className="text-6xl text-gray-500 font-bold">404</h2>
@@ -6,6 +14,12 @@ const ErrorPage = () => {
       <p className="text-lg text-center">
         The resource requested could not be found on this server!
       </p>
+      <button
+        className="bg-red-500 py-2 px-3 text-white font-bold rounded-md"
+        onClick={handleGoBack}
+      >
+        Go Back
+      </button>
     </div>
   );
 };
